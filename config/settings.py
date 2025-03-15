ecd31dae38669450227f9209b21adccced445a01
Mahan Mazaheri <mahan78ir@gmail.com>
2025-03-15 12:37:53 +0330

    Add postgres to project

--- config/settings.py
+++ config/settings.py
@@ -86,10 +86,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "weatherAPI",
+        "USER": "mahanmazaheri",
+        "PASSWORD": "0022737944",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
